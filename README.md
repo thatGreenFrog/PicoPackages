114e34e01a58e3eb23c6eacb5796491d8ab9446c
martins <trubinsh@gmail.com>
2021-10-11 21:08:54 +0300

    Refactored HC05 class code, removed redundant code Added additional Exceptions for finer control. Added comments for better readability. Updated README with newest changes.

--- README.md
+++ README.md
@@ -7,8 +7,10 @@ This repository contains different packages, drivers and modules for Raspberry P
 HC05 is simple driver for HC-05 bluetooth module that implements simplified data reads and writes to module. Data is written with header that defines actual data message length delimited with colon (:).
 Sample data = 13:thatGreenFrog.
 #### Constructor parameters
-- READ_TIMEOUT - data read timeout in seconds.
-- BAUDRATE - currently not in use
+- state_pin_num - state pi number of HC-05 module. Used to check if connection has been established to another BT device. DEFAULT: None
+- CHECK_CONNECTION - boolean parameter. If set to false driver will not check connection prior to writing or reading data regardless if state_pin_num has been passed. If state_pin_num is not passed then CHECK_CONNECTION will be set to False. DEFAULT: True
+- BAUDRATE - currently not in use. DEFAULT: 9600
 #### Available methods
-- wait_for_data() - will wait for data to be present in HC05 buffer. Will throw bluetooth.TimeoutException if data is not present in buffer after {READ_TIMEOUT} seconds
-- send_data() - will write data to HC-05 module buffer to be sent to paired device.
+- read_data(READ_TIMEOUT = 60) - Will block current thread until any data is read from buffer or READ_TIMEOUT is exceeded. If state_pin_num is passed and CHECK_CONNECTION set to True method will wait until connection is established.
+- send_data() - will write data to HC-05 module buffer to be sent to paired device. If state_pin_num is passed and CHECK_CONNECTION set to True method will wait until connection is established.
+- wait_for_connection(WAIT_CONNECTION_TIMEOUT = 60) - method will wait until connection is established with another BT device (state_pin.value() == 1) and either raise ConnectionTimeoutException if WAIT_CONNECTION_TIMEOUT is exceeded or unblock thread if connection is established. If state pin is not initialized, method will raise StatePinNotInitializedException
--- bluetooth.py
+++ bluetooth.py
@@ -1,56 +1,85 @@
-from machine import Pin,UART,ADC
+from machine import Pin,UART
 import utime
 
 
 class HC05:
     
     uart = None
-    READ_TIMEOUT = None
+    state_pin = None
+    CHECK_CONNECTION = None
     
-    def __init__(self, READ_TIMEOUT = 60, BAUDRATE = 9600):
-        self.READ_TIMEOUT = READ_TIMEOUT
+    def __init__(self, state_pin_num = None, CHECK_CONNECTION = True, BAUDRATE = 9600):
+        self.CHECK_CONNECTION = CHECK_CONNECTION and state_pin_num is not None
         self.uart = UART(0, BAUDRATE)
-        
-        
+        if state_pin_num is not None:
+            self.state_pin = Pin(state_pin_num, Pin.IN)
+
+
+    # Actual method to read data from buffer
     def _read_data(self):
-        data = b""
+        if self.CHECK_CONNECTION:
+            # If state_pin is initialized and CHECK_CONNECTION passed to constructor as True 
+            # method will wait until connection is estableshed to another BT device
+            self.wait_for_connection()
+        data = None
         if self.uart.any():
-            header = self._read_header()
             data = b""
-            read = self.uart.read(int(header))
+            read = self.uart.read()
             if not read == None:
                 data = b"".join([data, read])
-        return data.decode('utf-8')
-    
-    
-    def _read_header(self):
-        header = b""
-        while True:
-            read = self.uart.read(1)
-            if read == b":":
-                break
-            elif not read == None:
-                header = b"".join([header, read])
-        return header.decode('utf-8')
+        return data.decode('utf-8') if data is not None else None
 
 
-    def wait_for_data(self):
-        data = ""
+    # Read data from buffer. If READ_TIMEOUT > 0 method will block thread until data is received or timeout exceeded
+    def read_data(self, READ_TIMEOUT = 60):
+        data = None
         start_time = utime.ticks_ms()
-        while len(data) < 1:
-            if utime.ticks_ms() - start_time > self.READ_TIMEOUT * 1000:
-                raise TimeoutException()
-            data = self._read_data()
-        return data
+        while True:
+            data = self.read_data()
+            if READ_TIMEOUT == 0 or len(data) > 0:
+                # If any data has been read from buffer then we return this data immidiately
+                # or if READ_TIMEOUT has been set to 0, then method assumes that the user 
+                # doesn't want to block the thread and returns data regardless if anything has been read from buffer
+                return data
+            elif utime.ticks_ms() - start_time > READ_TIMEOUT * 1000:
+                # If READ_TIMEOUT > 0, then we raise ReadTimeoutException (if the timeout has been exceede)
+                # so that user can handle this gracefully
+                raise ReadTimeoutException()
     
-    def _write_data(self, data):
-        str_data = str(data)
-        self.uart.write(str(len(str_data)) + ":" + str_data)
-        
-        
+    # Write data to buffer.
     def send_data(self, data):
-        self._write_data(data)
+        if self.CHECK_CONNECTION:
+            # If state_pin is initialized and CHECK_CONNECTION passed to constructor as True 
+            # method will wait until connection is estableshed to another BT device
+            self.wait_for_connection()
+        self.uart.write(str(data))
 
 
-class TimeoutException(Exception):
+    # When HC-05 modukle state pin returns value 1 it means that connection has been established to another BT device
+    # This method will block current thread until state pin returns value 1 or WAIT_CONNECTION_TIMEOUT timeout is exceeded 
+    def wait_for_connection(self, WAIT_CONNECTION_TIMEOUT = 60):
+        if self.state_pin == None:
+            # If state pin is not initialized then we cannot check connection
+            # for this reason exception is raised, so that user can handle gracefully
+            raise StatePinNotInitializedException()
+        start_time = utime.ticks_ms()
+        while not self.state_pin.value() == 1:
+            if utime.ticks_ms() - start_time > WAIT_CONNECTION_TIMEOUT * 1000:
+                raise ConnectionTimeoutException()
+            utime.sleep(0.1)
+
+
+class HC05Exception(Exception):
+    pass
+
+
+class ReadTimeoutException(HC05Exception):
+    pass
+
+
+class ConnectionTimeoutException(HC05Exception):
+    pass
+
+
+class StatePinNotInitializedException(HC05Exception):
     pass
